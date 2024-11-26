6ba9d537228798d68dca4c30e278194b78626aad
tamalla <theo.amalla@lyceeastier.com>
2024-11-26 15:32:22 +0100

    commit

--- src/Ipv4.java
+++ src/Ipv4.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Ipv4 {
+    private String _192_a_223 = "(19[2-9]|2[0-2][0-3])";
+    private String adressesPrivees;
+    private String _0_a_255 = "([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
+    private String apipa;
+    private String _0_a_126 = "([0-9]|[1-9][0-9]|1[0-1][0-9]|12[0-6])";
+    private String _128_a_191 = "(12[8-9]|1[3-8][0-9]|19[0-1])";
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import clavier.In;
+
 public class Main {
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        String ClasseIpV4 = new String ("0");
+
+        System.out.println("Veuillez entrer une adresse IPv4");
+        ClasseIpV4 = In.readString();
     }
 }
