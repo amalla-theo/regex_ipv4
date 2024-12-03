4bf58dd77989a399525d3be7eb4beba2cc89c947
tamalla <theo.amalla@lyceeastier.com>
2024-12-03 16:46:19 +0100

    rien ne marche :)

--- src/Ipv4.java
+++ src/Ipv4.java
@@ -4,33 +4,17 @@ public class Ipv4 {
     // Les regex de ce paragraphes sont fonctionnels
     private String _192_a_223 = "(19[2-9]|2[0-2][0-3])";
     private String _0_a_255 = "([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
-    private String _0_a_126 = "([0-9]|[1-9][0-9]|1[0-1][0-9]|12[0-6])";
+    private String _0_a_127 = "([0-9]|[1-9][0-9]|1[0-1][0-9]|12[0-7])";
     private String _128_a_191 = "(12[8-9]|1[3-8][0-9]|19[0-1])";
 
 
     private String adressesPrivees;
     private String apipa;
 
-/*
-    public boolean classeA (String _0_a_255, String classeIpV4) {
-        boolean classeA = false;
-        if (classeIpV4.matches(_0_a_255)) {
-            classeA = true;
-        }
-        return classeA;
-    }
 
-*/
 
-/*
-    public class Test ((String _0_a_255, String classeIpV4)){
-        boolean bTest = false;
-        if (classeIpV4.matches(_0_a_255)) {
-            bTest = true;
-        }
-        return bTest;
+    public boolean classeA(String ip) {
+    String regexA =  _0_a_127 + "\\." + _0_a_255 + "\\." + _0_a_255 + "\\." + _0_a_255;
+    return ip.matches(regexA);
     }
 }
-
-
- */
--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,21 @@ import clavier.In;
 
 public class Main {
 
-
     public static void main(String[] args) {
 
+        Ipv4 saisie = new Ipv4();
 
         String classeIpV4 = new String("0");
 
         System.out.println("Veuillez entrer une adresse IPv4");
         classeIpV4 = In.readString();
 
-        Ipv4 c = new Ipv4();
+
+        if (saisie.classeA(classeIpV4) == true) {
+            System.out.println("Cette adresse est de classe A");
+        }else {
+            System.out.println("Erreur");
+        }
 
     }
 }
