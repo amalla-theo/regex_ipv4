62e308bb0b0f55fb8fe87be5886e9c70e5a6555c
tamalla <theo.amalla@lyceeastier.com>
2024-12-03 15:40:06 +0100

    rien ne marche :)

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,15 @@ import clavier.In;
 public class Main {
 
 
-
     public static void main(String[] args) {
 
-        String ClasseIpV4 = new String ("0");
+
+        String classeIpV4 = new String("0");
 
         System.out.println("Veuillez entrer une adresse IPv4");
-        ClasseIpV4 = In.readString();
+        classeIpV4 = In.readString();
+
+        Ipv4 c = new Ipv4();
+
     }
 }
